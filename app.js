86afa1a222306da5db1523a129dee435f4ec1008
gary909 <garywhite61@hotmail.com>
2022-11-09 12:46:27 +0100

    code

---
+++ app.js
@@ -0,0 +1,7 @@
+function twoDecimalPlaces(n) {
+    // Your code here
+}
+
+console.log(twoDecimalPlaces(4.659725356)); // 4.66
+console.log(twoDecimalPlaces(173735326.3783732637948948)); // 173735326.38
+console.log(twoDecimalPlaces(4.653725356)); // 4.65
